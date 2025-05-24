15903a2ecd0c0939478a5298df6e6a2ebe2a9d72
Karshoul <Khuongmingo@gmail.com>
2025-05-24 17:45:10 +0700

    split code

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const { getHomePage, getABC } = require('../controllers/homeController');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    // res.send('Hello world')
-    res.render('sample.ejs') // Gọi tới file sample.ejs trong thư mục views
-})
+//router.Method('/route', handler)
 
-router.get('/abc', (req, res) => {
-    res.send('Test 123')
-})
+router.get('/', getHomePage);
+router.get('/abc', getABC);
 
 module.exports = router; // export default - biến router đặt tên gì cũng đc nó sẽ tự động mapping tới biến xuất ra ngoài
