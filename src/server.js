1d438b4b3c464fe6a158521bee274560e6b1412f
Karshoul <Khuongmingo@gmail.com>
2025-05-24 21:41:28 +0700

    connect to DB

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+
+// Get the client
+const mysql = require('mysql2');
 // import express from 'express'; // es modules
 
 const app = express(); // app express
@@ -15,6 +18,30 @@ configViewEngine(app);
 
 // Khai báo route
 app.use('/', webRoutes);
+
+//test connection 
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, //default: 3306
+    user: 'root', //default: empty
+    password: '123456', //default: empty
+    database: 'hoidanit',
+  });
+
+// A simple SELECT query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+      console.log(">>> results= " ,results); // results contains rows returned by server
+      console.log(">>> fields= " ,fields); // fields contains extra meta data about results, if available
+    }
+); 
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+  });
 
 
 app.listen(port, hostname, () => {
